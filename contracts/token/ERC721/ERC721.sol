f8ce65ba084b6688b330b05593bf2e8f5255f007
Mikhail <m.dobrokhvalov@gmail.com>
2021-08-05 15:51:25 +0300

    Update ERC721.sol

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -40,9 +40,11 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
+    public address factory;
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
+        factory = msg.sender;
     }
 
     /**
@@ -249,6 +251,11 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      */
     function _safeMint(address to, uint256 tokenId) internal virtual {
         _safeMint(to, tokenId, "");
+    }
+    
+    function mint(address to, uint256 tokenId) public virtual {
+        require(msg.sender === factory, 'not factory');
+        _safeMint(to, tokenId);
     }
 
     /**
